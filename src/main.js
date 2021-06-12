c1af44b7797066b9d19bdaa582d6a72dd0499509
arpalanca <alseusrobertpalanca@gmail.com>
2021-06-12 19:53:59 +0800

    Initialize google map API

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app
+  .use(VueGoogleMaps, {
+    load: {
+      key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg",
+    },
+  })
+  .mount("#app");
